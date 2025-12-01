dab016e6c83cbc95627e3339444deb2c0b7c5d78
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-01 20:06:48 +0000

    Fixed the script of 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
 
-/* main function already provided in the template */
+/**
+ * main - Prints whether a randomly generated number is positive, zero or negative
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int n;
@@ -10,13 +14,18 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2;
 
-    /* Add your code below */
     if (n > 0)
+    {
         printf("%d is positive\n", n);
+    }
     else if (n == 0)
+    {
         printf("%d is zero\n", n);
+    }
     else
+    {
         printf("%d is negative\n", n);
+    }
 
     return (0);
 }
