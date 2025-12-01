7ca11e177bcc05a836a64b51e0dfef967dd5bb32
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-01 06:45:48 +0000

    Add script to 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message using printf.
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
