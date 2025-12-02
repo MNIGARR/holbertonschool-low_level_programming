6b0689522aeba59ecf6ad9e6fe2b71f34c6ad478
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-02 07:49:43 +0000

    Change some lines

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 
     char c;
 
-    for (c='a'; c <= 'z'; c++) {
+    for (c = 'a'; c <= 'z'; c++) 
         putchar(c);
-    }
-    for (c='A'; c <='Z'; c++) {
+
+    for (c = 'A'; c <= 'Z'; c++) 
         putchar(c);
-    }
 
     putchar("\n");
 
