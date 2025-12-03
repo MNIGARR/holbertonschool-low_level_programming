89b847867b1c9a27824bbb618cf55a29f39e353a
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-03 09:25:16 +0000

    Fixed last time

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,9 @@ int main(void)
 {
     char c;
 
-    /* print lowercase */
     for (c = 'a'; c <= 'z'; c++)
         putchar(c);
 
-    /* print uppercase */
     for (c = 'A'; c <= 'Z'; c++)
         putchar(c);
 
