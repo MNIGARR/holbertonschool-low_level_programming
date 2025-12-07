39710005fafd8fd61f0d12cb51ae5274f1b45e06
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-07 20:00:29 +0000

    Fixed betty coding style

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,33 +5,29 @@
  */
 void times_table(void)
 {
-    int i, j, prod, tens, ones;
+	int i, j, prod, tens, ones;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            prod = i * j;
-            tens = prod / 10;
-            ones = prod % 10;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			tens = prod / 10;
+			ones = prod % 10;
 
-            if (j == 0)
-            {
-                _putchar(ones + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-
-                if (prod < 10)
-                    _putchar(' '); /* extra space for alignment */
-                else
-                    _putchar(tens + '0');
-
-                _putchar(ones + '0');
-            }
-        }
-        _putchar('\n');
-    }
+			if (j == 0)
+				_putchar(ones + '0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (prod < 10)
+					_putchar(' ');
+				else
+					_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
