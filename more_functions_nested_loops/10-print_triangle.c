9b3254bb3a966b63891e9191c85fc249bb17e8cb
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-09 09:36:54 +0000

    Add script to 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using '#'
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+    int row, col;
+
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (row = 1; row <= size; row++)
+    {
+        /* print spaces first */
+        for (col = 1; col <= size - row; col++)
+            _putchar(' ');
+
+        /* then print #'s */
+        for (col = 1; col <= row; col++)
+            _putchar('#');
+
+        _putchar('\n');
+    }
+}
