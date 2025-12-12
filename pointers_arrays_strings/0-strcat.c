12793f3c2a0454f4ac3ef01dcbb4f5ff5d09ff7c
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-12 07:44:17 +0000

    Change code for betty

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,11 @@ char *_strcat(char *dest, char *str) {
     int i = 0;
     int j = 0;
 
+    /* find the end of dest */
     while (dest[i] != '\0')
         i++;
-    
+
+    /* append src to dest */
     while (str[j] != '\0')
     {
         dest[i] = str[j];
@@ -21,6 +23,7 @@ char *_strcat(char *dest, char *str) {
         j++;
     }
 
+    /* add terminating null byte */
     dest[i] = '\0';
 
     return (dest);
