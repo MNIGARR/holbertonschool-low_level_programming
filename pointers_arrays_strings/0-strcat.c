e3f282ee58e1e55eac9dcd31612efc1127d48406
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-12 07:45:53 +0000

    Changed it second time

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-    * _strcat - concatenates two strings
-    * @dest: destination string
-    * @src: source string
-    *
-    * Return: pointer to dest
-*/
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
 char *_strcat(char *dest, char *str) {
     int i = 0;
     int j = 0;
