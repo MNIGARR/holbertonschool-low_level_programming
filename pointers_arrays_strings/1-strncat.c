b93c4de5c874053f50c053e14d59a59d3aabaa3c
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-13 15:40:31 +0000

    Fixed the problem about in main.h 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,20 +10,23 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0;
-    int j = 0;
+	int i;
+	int j;
 
-    while (dest[i] != '\0')
-        i++;
+	i = 0;
+	j = 0;
 
-    while (src[j] != '\0' && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (dest[i] != '\0')
+		i++;
 
-    dest[i] = '\0';
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    return (dest);
+	dest[i] = '\0';
+
+	return (dest);
 }
