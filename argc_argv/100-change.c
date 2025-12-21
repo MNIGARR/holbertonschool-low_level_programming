dac44c6c28cf80d25ad38addd4e7b550a640e792
MNIGARR <nigarrmustafazada@gmail.com>
2025-12-21 17:05:11 +0000

    Add script to 100-change.c

---
+++ argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to make change
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+	int values[] = {25, 10, 5, 2, 1};
+	int i = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (cents > 0 && i < 5)
+	{
+		coins += cents / values[i];
+		cents %= values[i];
+		i++;
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
