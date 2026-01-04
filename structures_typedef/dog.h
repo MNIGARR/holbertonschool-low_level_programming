bf85634912137210320af6e6f70f4f3cfac120c4
MNIGARR <nigarrmustafazada@gmail.com>
2026-01-04 18:48:13 +0000

    Add comment to the dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+
 
 #endif
